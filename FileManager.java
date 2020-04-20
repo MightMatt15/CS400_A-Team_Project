ea9ae0fb85af245eeb1395dce8cbb00532fbf3f3
Matt McNaught <mmcnaught@wisc.edu>
2020-04-20 15:45:32 -0500

    test for git

--- FileManager.java
+++ FileManager.java
@@ -1,4 +1,14 @@
+package cs400ATeamFinalProject;
 
 public class FileManager {
 
+  public String inputFile;
+  public String outputFile;
+  
+  
+  
+  public boolean readFile() {
+    
+    return false;
+  }
 }
