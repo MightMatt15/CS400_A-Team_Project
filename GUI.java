e56f4f22ea561875f7fde371ab301a4cc2c8d216
zyleei <43762477+zyleei@users.noreply.github.com>
2020-04-20 20:20:20 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -16,6 +16,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -35,32 +36,25 @@ public class GUI extends Application {
 		// args = this.getParameters().getRaw();
 
 		BorderPane panel = new BorderPane();
-		
-		Label title  = new Label("Milk Production Dashboard\n   -- Chalet Cheese Factory");
+
+		Label title = new Label("Milk Production Dashboard\n   -- Chalet Cheese Factory");
 		title.setFont(Font.font("Marker Felt", FontWeight.EXTRA_BOLD, 25));
 		panel.setTop(title);
 		BorderPane.setAlignment(title, Pos.CENTER);
-		
+
 		VBox selectionPanel = new VBox();
-		Label selection = new Label("Filters");
-		selection.setFont(Font.font("Chalkduster", FontWeight.BOLD, 20));
-		Label year = new Label("Year");
-		year.setFont(Font.font("Times New Roman", FontWeight.BOLD, 15));
+		Label selection = createLabel("Filters", "Chalkduster", FontWeight.BOLD, 20);
+		Label year = createLabel("Year", "Times New Roman", FontWeight.BOLD, 15);
 		TextField yearInput = new TextField();
-		Label month = new Label("Month");
-		month.setFont(Font.font("Times New Roman", FontWeight.BOLD, 15));
+		Label month = createLabel("Month", "Times New Roman", FontWeight.BOLD, 15);
 		TextField monthInput = new TextField();
-		Label day = new Label("Day");
-		day.setFont(Font.font("Times New Roman", FontWeight.BOLD, 15));
+		Label day = createLabel("Day", "Times New Roman", FontWeight.BOLD, 15);
 		TextField dayInput = new TextField();
-		Label farm = new Label("Farm");
-		farm.setFont(Font.font("Times New Roman", FontWeight.BOLD, 15));
+		Label farm = createLabel("Farm", "Times New Roman", FontWeight.BOLD, 15);
 		TextField farmInput = new TextField();
-		
+
 		Button search = new Button("SEARCH");
-		
-	
-		
+
 		selectionPanel.getChildren().add(selection);
 		selectionPanel.getChildren().add(year);
 		selectionPanel.getChildren().add(yearInput);
@@ -72,11 +66,51 @@ public class GUI extends Application {
 		selectionPanel.getChildren().add(farmInput);
 		selectionPanel.getChildren().add(search);
 		panel.setLeft(selectionPanel);
-		
+
+		VBox resultsPanel = new VBox();
+		GridPane results = new GridPane();
+		results.add(createLabel("Total:	", "Times New Roman", FontWeight.BOLD, 15), 0, 0);
+		results.add(createLabel("Maximum:	", "Times New Roman", FontWeight.BOLD, 15), 0,
+				1);
+		results.add(createLabel("Minimum:	", "Times New Roman", FontWeight.BOLD, 15), 0,
+				2);
+		results.add(createLabel("Average:	", "Times New Roman", FontWeight.BOLD, 15), 0,
+				3);
+		results.add(
+				createLabel("Percentage:	", "Times New Roman", FontWeight.BOLD, 15), 0,
+				4);
+		for (int i = 0; i < 6; i++)
+			results.add(new TextField(), 1, i);
+		results.add(new TextField("e.g: farm 01"), 2, 2);
+		results.add(new TextField("e.g: farm 02"), 2, 3);
+		results.add(new Label("(out of all farms)"), 2, 4);
+		results.add(new Label("(out of the whole time period)"), 2, 5);
+
+		resultsPanel.getChildren()
+				.add(createLabel("Results", "Chalkduster", FontWeight.BOLD, 20));
+		resultsPanel.getChildren().add(results);
+		results.setAlignment(Pos.TOP_LEFT);
+		panel.setCenter(resultsPanel);
+
 		Scene mainScene = new Scene(panel, WINDOW_WIDTH, WINDOW_HEIGHT);
 		// Add the stuff and set the primary stage
 		primaryStage.setScene(mainScene);
 		primaryStage.show();
+	}
+
+	/**
+	 * Create a label with specified font, font weight and size.
+	 * 
+	 * @param label  content of the label
+	 * @param font   font
+	 * @param weight font weight
+	 * @param size   size of font
+	 * @return a Label reference
+	 */
+	private Label createLabel(String label, String font, FontWeight weight, int size) {
+		Label newLabel = new Label(label);
+		newLabel.setFont(Font.font(font, weight, size));
+		return newLabel;
 	}
 
 	/**
