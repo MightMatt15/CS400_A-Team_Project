74f2402db65cb052a55f8f31ee381e12b62eeae6
Matt McNaught <mmcnaught@wisc.edu>
2020-04-21 21:51:53 -0500

    added code to make Main the main file

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
 package application;
 
+import javafx.stage.Stage;
 
 public class Main {
 
+  
+  
+  public void main(String args[]) {
+    //create and start the GUI
+    GUI gui = new GUI();
+    try {
+      gui.start(new Stage());
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+    }
+    
+  }
 }
