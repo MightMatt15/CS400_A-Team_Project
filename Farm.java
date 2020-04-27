a089398c591086eeb79ceca4f3decc42fdd32b26
zyleei <43762477+zyleei@users.noreply.github.com>
2020-04-27 17:10:37 -0500

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -1,106 +1,185 @@
 package application;
 
+import java.util.ArrayList;
 
 public class Farm {
-  
-  public String farmID;
-  public String owner;
-  public BST<String, Data> milkData;           //TODO data structure
-  
-  
-  //constructor to initialize a new farm
-  public Farm(String farmID, String owner) {
-    this.farmID = farmID;
-    this.owner = owner;
-  }
-  
-  /*
-   * A class to store a year's worth of data
-   */
-  private class Data{
-    private Integer january[];
-    private Integer february[];
-    private Integer march[];
-    private Integer april[];
-    private Integer may[];
-    private Integer june[];
-    private Integer july[];
-    private Integer august[];
-    private Integer september[];
-    private Integer october[];
-    private Integer november[];
-    private Integer december[];
-    
-    private Data() {
-      january = new Integer[31];
-      february = new Integer[29];
-      march = new Integer[31];
-      april = new Integer[30];
-      may = new Integer[31];
-      june = new Integer[30];
-      july = new Integer[31];
-      august = new Integer[31];
-      september = new Integer[30];
-      october = new Integer[31];
-      november = new Integer[30];
-      december = new Integer[31];
-    }
-  }
-  
-  /*
-   * Inserts data for milk on a specific date
-   * 
-   * @throws ArrayIndexOutOfBoundsException if the day to access is invalid
-   * 
-   * @param year year the date is in
-   * @param month month the date to edit is in
-   * @param day day to edit
-   * @param weight weight of the milk for the day
-   * @returns true if inserted successfully
-   * 
-   */
-  public boolean insertMilkForDate(int year, String month, int day, Integer weight) throws ArrayIndexOutOfBoundsException {
-    try {
-      
-      //TODO check if day/month/year is already in tree???
-      //cant just add a new year if it is there already
-      //maybe start by adding a year if it is found in the file and init to 0
-      //then check if year exists and check if day is nonzero??
-      //could use a method to find the day in the tree and edit it?
-      Data data = new Data();
-      milkData.insert(farmID, data);
-    } catch (IllegalNullKeyException e) {
-      return false;
-    } catch (DuplicateKeyException e) {
-      return false;
-    }
-    
-    return true;
-  }
-  
-  
-  public boolean editMilkForDate() {
-    
-    
-    return false;
-  }
-  
-  
-  public Object removeMilkForDate() {
-    
-    return null;
-  }
-  
-  
-  public Object clearData() {
-    
-    return null;
-  }
-  
-  
-  
-  
-  
-  
-  
+	private String farmID; // farm ID can be any string
+	private String owner;
+	private ArrayList<annualData> yearList;
+
+	/**
+	 * Constructor to initialize a new farm
+	 * 
+	 * @param farmID farm identifier
+	 * @param owner  owner of the farm
+	 */
+	public Farm(String farmID, String owner) {
+		this.farmID = farmID;
+		this.owner = owner;
+		yearList = new ArrayList<annualData>();
+	}
+
+	private class annualData {
+		private int year;
+		private int annualSum;
+		private int[][] annualData;
+		private int[] monthlySum;
+
+		private annualData() {
+			annualData = new int[12][31];
+			monthlySum = new int[12];
+		}
+
+		private void addData(int month, int day, int weight) {
+			annualData[month - 1][day - 1] += weight;
+			annualSum += weight;
+			monthlySum[month - 1] += weight;
+		}
+
+		private int getData(int month, int day) {
+			return annualData[month - 1][day - 1];
+		}
+
+		private int getAnnualSum() {
+			return annualSum;
+		}
+
+		private int getMonthlySum(int month) {
+			return monthlySum[month - 1];
+		}
+
+		private int getIntervalSum(int mStart, int mEnd, int dStart, int dEnd) {
+			int sum = 0;
+			int m = 0;
+			int d = dStart - 1;
+			for (m = mStart - 1; m < 12; m++) {
+				for (; d < 31; d++) {
+					if (m == mEnd && d == dEnd)
+						return sum;
+					sum += annualData[m][d];
+				}
+				d = 0;
+			}
+			return -1; // fail
+		}
+
+		private int getYear() {
+			return year;
+		}
+	}
+
+	/**
+	 * Inserts data for milk on a specific date given that all arguments are valid.
+	 * 
+	 * @param year   year the date is in
+	 * @param month  month the date to edit is in
+	 * @param day    day to edit
+	 * @param weight weight of the milk for the day
+	 * @returns true if inserted successfully
+	 * @throws ArrayIndexOutOfBoundsException if the day to access is invalid
+	 */
+	public boolean insertMilkForDate(int year, int month, int day, int weight)
+			throws ArrayIndexOutOfBoundsException {
+		annualData reportofYear = yearExists(year);
+		if (reportofYear == null) {
+			annualData mdata = new annualData();
+			mdata.addData(month, day, weight);
+			yearList.add(mdata);
+		} else {
+			reportofYear.addData(month, day, weight);
+		}
+		return true;
+	}
+
+	public int sumWithinRange(int year1, int month1, int day1, int year2, int month2,
+			int day2) {
+		int sum = 0;
+		int[] nextDate;
+		annualData year = null;
+		// date 1 is the start date, date 2 is the end date
+		while (dateToInt(year1, month1, day1) <= dateToInt(year2, month2, day2)) {
+			year = yearExists(year1);
+			if (year == null) { // year does not exist; go to the next year
+				year1++;
+				month1 = 1;
+				day1 = 1;
+				continue;
+			}
+			sum += year.getData(month1, day1);
+			nextDate = nextDate(year1, month1, day1); // increment the date
+			year1 = nextDate[0];
+			month1 = nextDate[1];
+			day1 = nextDate[2];
+		}
+		return sum;
+	}
+
+	private int dateToInt(int year, int month, int day) {
+		return year * 10000 + month * 100 + day;
+	}
+
+	private int[] nextDate(int year, int month, int day) {
+		int[] nextDate = new int[3];
+
+		if ((day + 1) > 31)
+			if ((month + 1) > 12) {
+				year++;
+				month = 1;
+				day = 1;
+			} else {
+				month++;
+				day = 1;
+			}
+		else
+			day++;
+
+		nextDate[0] = year;
+		nextDate[1] = month;
+		nextDate[2] = day;
+
+		return nextDate;
+	}
+
+	private boolean sameDate(int year1, int month1, int day1, int year2, int month2,
+			int day2) {
+		return (day1 == day2) && (month1 == month2) && (year1 == year2);
+	}
+
+	/**
+	 * Determine if the annual report has been created.
+	 * 
+	 * @param year the year to be found in the year list
+	 * @return true the index of the annual report exists; -1 otherwise
+	 */
+	private annualData yearExists(int year) {
+		for (annualData a : yearList)
+			if (a.getYear() == year)
+				return a;
+		return null;
+	}
+
+	public boolean editMilkForDate() {
+
+		return false;
+	}
+
+	public Object removeMilkForDate() {
+
+		return null;
+	}
+
+	public Object clearData() {
+
+		return null;
+	}
+
+	/**
+	 * Return the identifier of the farm.
+	 * 
+	 * @return the identifier of the farm
+	 */
+	public String getFarmID() {
+		return farmID;
+	}
+
 }
