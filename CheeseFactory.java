4a300142c87e17c656a4f2bd0f43421ade14457c
zyleei <43762477+zyleei@users.noreply.github.com>
2020-04-30 09:32:50 -0500

    add yearList variable

--- CheeseFactory.java
+++ CheeseFactory.java
@@ -4,120 +4,127 @@ import java.util.ArrayList;
 
 public class CheeseFactory {
 
-    private ArrayList<Farm> milkDataFromFarms;
+	private ArrayList<Farm> milkDataFromFarms;
+	private ArrayList<Integer> yearList;
 
-    public CheeseFactory() {
-        milkDataFromFarms = new ArrayList<Farm>();
-    }
+	public CheeseFactory() {
+		milkDataFromFarms = new ArrayList<Farm>();
+		yearList = new ArrayList<Integer>();
+	}
 
-    
-    public ArrayList<Farm> getFarmList(){
-        return milkDataFromFarms;
-    }
-    
-        public int annualSumAllFarms(int year) {
-        int annualSum = 0;
-        for(Farm f : milkDataFromFarms) {
-            annualSum += f.annualSum(year);
-        }
-        return annualSum;
-    }
-    
-    /**
-     * Inserts a single data point on a given day
-     * 
-     * @param farmID unique ID of the farm
-     * @param year   year the date is in
-     * @param month  month the date to insert is in
-     * @param day    day to edit
-     * @param weight weight of the milk for the day
-     */
-    public void insertSingleData(String farmID, int year, int month, int day,
-            int weight) {
-        // if farm does not exist, add it
-        Farm farm = getFarm(farmID);
-        if (farm == null) {
-            milkDataFromFarms.add(new Farm(farmID));
-        }
-        farm = getFarm(farmID);
-        farm.insertMilkForDate(year, month, day, weight);
-    }
+	public ArrayList<Farm> getFarmList() {
+		return milkDataFromFarms;
+	}
 
-    /**
-     * Get the farm specified by farmID.
-     * 
-     * @param farmID farm identifier
-     * @return farm reference; null if farm does not exist;
-     */
-    Farm getFarm(String farmID) {
-        for (Farm farm : milkDataFromFarms) {
-            if (farm.getFarmID().equals(farmID))
-                return farm;
-        }
-        return null;
-    }
+	public int annualSumAllFarms(int year) {
+		int annualSum = 0;
+		for (Farm f : milkDataFromFarms) {
+			annualSum += f.annualSum(year);
+		}
+		return annualSum;
+	}
 
-    /**
-     * Get the number of farms in the cheese factory.
-     * 
-     * @return the number of farms in the factory
-     */
-    public int getFactorySize() {
-        return milkDataFromFarms.size();
-    }
+	/**
+	 * Inserts a single data point on a given day
+	 * 
+	 * @param farmID unique ID of the farm
+	 * @param year   year the date is in
+	 * @param month  month the date to insert is in
+	 * @param day    day to edit
+	 * @param weight weight of the milk for the day
+	 */
+	public void insertSingleData(String farmID, int year, int month, int day,
+			int weight) {
+		// if farm does not exist, add it
+		Farm farm = getFarm(farmID);
+		if (farm == null) {
+			milkDataFromFarms.add(new Farm(farmID));
+		}
+		farm = getFarm(farmID);
+		farm.insertMilkForDate(year, month, day, weight);
+		if (!yearList.contains(year))
+			yearList.add(year);
+	}
+	
+	public ArrayList<Integer> getYearList(){
+		return yearList;
+	}
 
-    /**
-     * Edits a single data point on a given day
-     * 
-     * @param farmID unique ID of the farm
-     * @param year   year the date is in
-     * @param month  month the date to edit is in
-     * @param day    day to edit
-     * @param weight weight of the milk for the day to edit
-     * @returns true if edited successfully
-     */
-    public boolean editSingleData(String farmID, int year, int month, int day,
-            Integer weight) {
-        Farm farm = getFarm(farmID);
-        if (farm == null)
-            return false;
-        else {
-            farm.editWeight(year, month, day, weight);
-            return true;
-        }
-    }
+	/**
+	 * Get the farm specified by farmID.
+	 * 
+	 * @param farmID farm identifier
+	 * @return farm reference; null if farm does not exist;
+	 */
+	Farm getFarm(String farmID) {
+		for (Farm farm : milkDataFromFarms) {
+			if (farm.getFarmID().equals(farmID))
+				return farm;
+		}
+		return null;
+	}
 
-    /**
-     * Remove a single data point on a given day
-     * 
-     * @param farmID unique ID of the farm
-     * @param year   year the date is in
-     * @param month  month the date to remove is in
-     * @param day    day to edit
-     * @returns the data that was removed; -1 if removal unsuccessful
-     */
-    public int removeSingleData(String farmID, int year, int month, int day) {
-        Farm farm = getFarm(farmID);
-        if (farm == null)
-            return -1;
-        else
-            return farm.removeMilkForDate(year, month, day);
-    }
+	/**
+	 * Get the number of farms in the cheese factory.
+	 * 
+	 * @return the number of farms in the factory
+	 */
+	public int getFactorySize() {
+		return milkDataFromFarms.size();
+	}
 
-    /**
-     * Return a single data point on a given day
-     * 
-     * @param farmID unique ID of the farm
-     * @param year   year the date is in
-     * @param month  month the date to get is in
-     * @param day    day to edit
-     * @returns the data that was retrieved or -1 if the farm was not found
-     */
-    public int getSingleData(String farmID, int year, int month, int day) {
-        Farm farm = getFarm(farmID);
-        if (farm == null)
-            return -1;
-        else
-            return farm.getSingleData(year, month, day);
-    }
+	/**
+	 * Edits a single data point on a given day
+	 * 
+	 * @param farmID unique ID of the farm
+	 * @param year   year the date is in
+	 * @param month  month the date to edit is in
+	 * @param day    day to edit
+	 * @param weight weight of the milk for the day to edit
+	 * @returns true if edited successfully
+	 */
+	public boolean editSingleData(String farmID, int year, int month, int day,
+			Integer weight) {
+		Farm farm = getFarm(farmID);
+		if (farm == null)
+			return false;
+		else {
+			farm.editWeight(year, month, day, weight);
+			return true;
+		}
+	}
+
+	/**
+	 * Remove a single data point on a given day
+	 * 
+	 * @param farmID unique ID of the farm
+	 * @param year   year the date is in
+	 * @param month  month the date to remove is in
+	 * @param day    day to edit
+	 * @returns the data that was removed; -1 if removal unsuccessful
+	 */
+	public int removeSingleData(String farmID, int year, int month, int day) {
+		Farm farm = getFarm(farmID);
+		if (farm == null)
+			return -1;
+		else
+			return farm.removeMilkForDate(year, month, day);
+	}
+
+	/**
+	 * Return a single data point on a given day
+	 * 
+	 * @param farmID unique ID of the farm
+	 * @param year   year the date is in
+	 * @param month  month the date to get is in
+	 * @param day    day to edit
+	 * @returns the data that was retrieved or -1 if the farm was not found
+	 */
+	public int getSingleData(String farmID, int year, int month, int day) {
+		Farm farm = getFarm(farmID);
+		if (farm == null)
+			return -1;
+		else
+			return farm.getSingleData(year, month, day);
+	}
 }
