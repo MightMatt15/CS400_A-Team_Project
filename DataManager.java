8a19ceea4d6271017f29470752e31271f4124a26
zyleei <43762477+zyleei@users.noreply.github.com>
2020-04-30 00:56:33 -0500

    Update DataManager.java

--- DataManager.java
+++ DataManager.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class DataManager {
 
-	private CheeseFactory factory;
+	public CheeseFactory factory;
 	private FileManager fmng;
 
 	/**
@@ -26,26 +26,8 @@ public class DataManager {
 		fmng = new FileManager();
 	}
 
-	
-	/**
-     * Reads the specified file and parses the data and inserts the data into the data structures in the program
-     * 
-     * @param inputFile the input file to read from
-     * @return true if the file was read correctly
-     * @throws IOException if the file is not able to be read
-     */
 	public boolean readFile(String inputFile) throws IOException {
 		return fmng.readFile(inputFile, factory);
-	}
-	
-	
-	/**
-     * Gets the factory
-     * 
-     * @return the cheese factory
-     */
-	public CheeseFactory getCheeseFactory() {
-	  return factory;
 	}
 
 	/**
@@ -132,35 +114,73 @@ public class DataManager {
 
 	public double getMonthlyMin(int year) {
 		double[] monthlyReport = getMonthlySumVec(year);
-		double monthlyMin = Double.MAX_VALUE;
-		for (int i = 0; i < 12; i++)
-			if (monthlyReport[i] < monthlyMin)
-				monthlyMin = monthlyReport[i];
-		return monthlyMin;
+		return getArrayMinValue(monthlyReport);
 	}
 
 	public double getMonthlyMax(int year) {
 		double[] monthlyReport = getMonthlySumVec(year);
-		double monthlyMax = Double.MIN_VALUE;
+		return getArrayMaxValue(monthlyReport);
+	}
+
+	private double getArrayMinValue(double[] array) {
+		double min = Double.MAX_VALUE;
 		for (int i = 0; i < 12; i++)
-			if (monthlyReport[i] > monthlyMax)
-				monthlyMax = monthlyReport[i];
-		return monthlyMax;
+			if (array[i] < min)
+				min = array[i];
+		return min;
 	}
 
-	public int getMonthlyAverageForFarm() {
-
-		return 0;
+	private double getArrayMaxValue(double[] array) {
+		double max = -1;
+		for (int i = 0; i < 12; i++)
+			if (array[i] > max)
+				max = array[i];
+		return max;
 	}
 
-	public int getMonthlyMinForFarm() {
-
-		return 0;
+	/**
+	 * Return the monthly report(monthly average) of a farm in the give year.
+	 * 
+	 * @param farmID farm identifier
+	 * @param year
+	 * @return a vector of length 12 representing the average amount of milk
+	 *         produced by this farm in every month of the year
+	 */
+	public double[] getMonthlyAverageForFarm(String farmID, int year) {
+		ArrayList<Farm> listOfFactories = factory.getFarmList();
+		for (Farm f : listOfFactories) {
+			if (f.getFarmID().equals(farmID))
+				return f.monthlyAvg(year);
+		}
+		return null; // the farm with the specified identifier was not found
 	}
 
-	public int getMonthlyMaxForFarm() {
+	/**
+	 * Return the monthly report(monthly sum) of a farm in the give year.
+	 * 
+	 * @param farmID farm identifier
+	 * @param year
+	 * @return a vector of length 12 representing the total amount of milk produced
+	 *         by this farm every month of the year
+	 */
+	public int[] getMonthlySumForFarm(String farmID, int year) {
+		ArrayList<Farm> listOfFactories = factory.getFarmList();
+		for (Farm f : listOfFactories) {
+			if (f.getFarmID().equals(farmID))
+				return f.monthlySum(year);
+		}
+		return null; // the farm with the specified identifier was not found
+	}
 
-		return 0;
+	public int getMonthlyMinForFarm(String farmID, int year) {
+		double[] monthlySumForFarm = intToDouble(getMonthlySumForFarm(farmID, year));
+		return (int) getArrayMinValue(monthlySumForFarm);
+
+	}
+
+	public int getMonthlyMaxForFarm(String farmID, int year) {
+		double[] monthlySumForFarm = intToDouble(getMonthlySumForFarm(farmID, year));
+		return (int) getArrayMaxValue(monthlySumForFarm);
 	}
 
 	public int getDataSortedByField() {
@@ -185,6 +205,23 @@ public class DataManager {
 
 	public int getSingleData(String farmID, int year, int month, int day) {
 		return factory.getSingleData(farmID, year, month, day);
+	}
+
+	public void writeFarmReport(String farmID, int year) {
+		for (int i = 0; i < farmMonthlyReport(year).length; i++) {
+			if (farmMonthlyReport(year)[i].farmID.equals(farmID)) {
+				System.out.println(farmMonthlyReport(year)[i]);
+			}
+		}
+	}
+
+	/**
+	 * Gets the factory
+	 * 
+	 * @return the cheese factory
+	 */
+	public CheeseFactory getCheeseFactory() {
+		return factory;
 	}
 
 }
